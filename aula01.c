a15f3803da5a6fef22bb428fa3192a84448cd625
Guest <lima.lucasdeoliveira@gmail.com>
2015-03-05 11:45:04 -0300

    Exercícios com arquivos

--- aula01.c
+++ aula01.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-main(){
-    exit(0);
+void main(){
+
+    FILE *f;
+    int matricula;
+    char nome[100];
+
+    printf("Informe a Matricula do aluno: ");
+    scanf("%i",&matricula);
+    printf("Informe o nome do aluno: ");
+    scanf("%s",&nome);
+
+    f = fopen("saida.txt", "w");
+    fprintf(f, "%i %s\n", matricula, nome);
+    fclose(f);
+
 }
