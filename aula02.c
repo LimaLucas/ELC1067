199097b24b334af591e73f398ae4723ec7ffbbd9
LimaLucas <lima.lucasdeoliveira@gmail.com>
2015-03-06 15:13:42 -0300

    Exercício para ler arquivo

--- aula02.c
+++ aula02.c
@@ -2,8 +2,16 @@
 
 void main(){
     FILE *f;
+    int matricula;
+    char nome[100];
 
+    f = fopen("saida.txt", "r");
+    if(f == NULL)
+        printf("Não foi possivel abrir o arquivo!")
+    else{
+        fscanf("%i %s", &matricula, &nome);
+        printf("Aluno: %s \n Matricula: %i", nome, matricula);
+    }
 
-
-
+    exit(0);
 }
