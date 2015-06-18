de5ca60d1882ac1ec6d2e4ea3466cec750b7ed3d
Lucas <lima.lucasdeoliveira@gmail.com>
2015-06-18 12:23:12 -0300

    Update T8

--- T8/main.c
+++ T8/main.c
@@ -9,7 +9,22 @@
 
 int main(int argc, char **argv)
 {
-	
+
+	if(argc == 1){
+ 		printf("Insira o nome do arquivo!\n");
+ 		return 0;
+ 	}else if(argc > 2){
+ 		printf("Insira apenas o nome de um arquivo!\n");
+ 		return 0;
+ 	}
+
+ 	FILE *file = fopen(argv[1], "r");
+ 	if(file == NULL){
+ 		printf("Erro ao abrir o arquivo! Tente novamente.\n");
+ 		return 0;
+ 	}
+
+ 	
 
 	memo_relatorio();
 	return 0;
